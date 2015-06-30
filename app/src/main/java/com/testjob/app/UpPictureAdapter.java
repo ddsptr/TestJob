d16b853f1230b773c097c170bb2b73e6eb00b72e
ddsptr <ddsmob@mail.ru>
2015-06-30 15:30:18 +0300

    Added testing pictures

--- app/src/main/java/com/testjob/app/UpPictureAdapter.java
+++ app/src/main/java/com/testjob/app/UpPictureAdapter.java
@@ -1,5 +1,6 @@
 package com.testjob.app;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.view.View;
 
@@ -7,9 +8,19 @@ import android.view.View;
  * Created by dds on 30.06.15.
  */
 public class UpPictureAdapter extends PagerAdapter {
+    private Context mContext;
+    private int [] mPictures = new int [] {
+            R.drawable.kudago1,
+            R.drawable.kudago2
+    };
+
+    UpPictureAdapter(Context context) {
+        mContext = context;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return mPictures.length;
     }
 
     @Override
