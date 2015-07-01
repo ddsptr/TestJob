2671ea31b213954e9421f57ee1b30f46c372bf16
ddsptr <ddsmob@mail.ru>
2015-07-01 09:43:42 +0300

    Added comment and sub articles list views

--- app/src/main/java/com/testjob/app/adapters/MainListAdapter.java
+++ app/src/main/java/com/testjob/app/adapters/MainListAdapter.java
@@ -8,9 +8,20 @@ import android.widget.BaseAdapter;
  * Created by dds on 01.07.15.
  */
 public class MainListAdapter extends BaseAdapter {
+    private static final int SECTIONS_COUNT = 3;
+
+    private static final int MAIN_ARTICLE_POSITION = 0;
+    private static final int SUB_ARTICLE_POSITION = 1;
+    private static final int COMMENT_POSITION = 2;
+
+    private static final int MAIN_ARTICLE_ID = 0;
+    private static final int SUB_ARTICLE_ID = 1;
+    private static final int COMMENT_ID = 2;
+
+
     @Override
     public int getCount() {
-        return 0;
+        return SECTIONS_COUNT;
     }
 
     @Override
@@ -20,7 +31,15 @@ public class MainListAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        switch (position) {
+            case MAIN_ARTICLE_POSITION:
+                return MAIN_ARTICLE_ID;
+            case SUB_ARTICLE_POSITION:
+                return SUB_ARTICLE_ID;
+            case COMMENT_POSITION:
+                return COMMENT_ID;
+        }
+        return -1; //todo decide about this unreachable code and throwing exception
     }
 
     @Override
